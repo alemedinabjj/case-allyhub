05f25bb666d19190f10f7ce47b3aa87c497c5ccd
Alexandre Medina <alesurf13@gmail.com>
2022-10-29 00:46:47 -0300

    fix bug

--- src/components/Input.tsx
+++ src/components/Input.tsx
@@ -2,22 +2,24 @@ import { forwardRef } from "react";
 
 export const InputBase = ({ label, name, type, error, ...rest }: any, ref: any) => {
   return (
-    <div className="flex flex-col gap-2">
+    <div className="flex flex-col gap-1">
       <label htmlFor={name} className="block text-sm font-medium text-gray-700">
         {label}
       </label>
-      <input
-        name={name}
-        id={name}
-        type={type}
-        className={`mt-1 block w-full py-2 px-3 border border-gray-300 rounded-md shadow-sm focus:outline-none focus:ring-indigo-500 focus:border-indigo-500 sm:text-sm ${
-          error ? "border-red-500" : ""
-        }
+      <div>
+        <input
+          name={name}
+          id={name}
+          type={type}
+          className={`mt-1 block w-full py-2 px-3 border border-gray-300 rounded-md shadow-sm focus:outline-none focus:ring-indigo-500 focus:border-indigo-500 sm:text-sm ${
+            error ? "border-red-500" : ""
+          }
         `}
-        ref={ref}
-        {...rest}
-      />
-      {error && <span className="text-red-500 text-xs">{error.message}</span>}
+          ref={ref}
+          {...rest}
+        />
+        {error && <span className="text-red-500 text-xs">{error.message}</span>}
+      </div>
     </div>
   );
 };
